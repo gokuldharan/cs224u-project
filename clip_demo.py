efcb7b48971bfca076657156d6a56d190dac1ce9
Virginia Adams <vadams@nvidia.com>
2021-06-01 21:29:13 -0700

    all expriment code

--- clip_demo.py
+++ clip_demo.py
@@ -11,6 +11,8 @@ text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 with torch.no_grad():
     image_features = model.encode_image(image)
     text_features = model.encode_text(text)
+    print(len(text_features[0]))
+    print(len(image_features[0]))
     
     logits_per_image, logits_per_text = model(image, text)
     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
